147b121a64f3fedc37511acdc888b5a7b71daa55
natalieghuey <hueyng@g.cofc.edu>
2025-11-13 13:06:33 -0500

    Add files via upload

--- School_Project_Part1.m
+++ School_Project_Part1.m
@@ -1,7 +1,6 @@
 %% School District Project
 
 data=readtable('Bussing_table.csv');
-data
 proportions = [0.32 0.38 0.3; 0.37 0.28 0.35; 0.3 0.32 0.38; 0.28 0.4 0.32;...
     0.39 0.34 0.27; 0.34 0.28, 0.38];
 Areatotals= [450 600 550 350 500 450];
@@ -36,4 +35,24 @@ prob.Constraints.school1 = x(1,1) + x(3,1) + x(4,1) + x(5,1) + x(6,1) <=900;
 prob.Constraints.school2 = x(1,2) + x(2,2) + x(3,2) + x(4,2) + x(6,2) <=1100;
 prob.Constraints.school3 = x(1,3) + x(2,3) + x(3,3) + x(5,3) + x(6,3) <=1000;
 %% 
-% Need to preserve proportions of each grade level:
+% Need to preserve proportions of each grade level at each school:
+prob.Constraints.school1grade6lower = dot(proportions(:,1),x(:,1)) >= 0.3*sum(x(:,1));
+prob.Constraints.school1grade6higher = dot(proportions(:,1),x(:,1)) <= 0.36*sum(x(:,1));
+prob.Constraints.school1grade7lower = dot(proportions(:,2),x(:,1)) >= 0.3*sum(x(:,1));
+prob.Constraints.school1grade7higher = dot(proportions(:,2),x(:,1)) <= 0.36*sum(x(:,1));
+prob.Constraints.school1grade8lower = dot(proportions(:,3),x(:,1)) >= 0.3*sum(x(:,1));
+prob.Constraints.school1grade8higher = dot(proportions(:,3),x(:,1)) <= 0.36*sum(x(:,1));
+prob.Constraints.school2grade6lower = dot(proportions(:,1),x(:,2)) >= 0.3*sum(x(:,2));
+prob.Constraints.school2grade6higher = dot(proportions(:,1),x(:,2)) <= 0.36*sum(x(:,2));
+prob.Constraints.school2grade7lower = dot(proportions(:,2),x(:,2)) >= 0.3*sum(x(:,2));
+prob.Constraints.school2grade7higher = dot(proportions(:,2),x(:,2)) <= 0.36*sum(x(:,2));
+prob.Constraints.school2grade8lower = dot(proportions(:,3),x(:,2)) >= 0.3*sum(x(:,2));
+prob.Constraints.school2grade8higher = dot(proportions(:,3),x(:,2)) <= 0.36*sum(x(:,2));
+prob.Constraints.school3grade6lower = dot(proportions(:,1),x(:,3)) >= 0.3*sum(x(:,3));
+prob.Constraints.school3grade6higher = dot(proportions(:,1),x(:,3)) <= 0.36*sum(x(:,3));
+prob.Constraints.school3grade7lower = dot(proportions(:,2),x(:,3)) >= 0.3*sum(x(:,3));
+prob.Constraints.school3grade7higher = dot(proportions(:,2),x(:,3)) <= 0.36*sum(x(:,3));
+prob.Constraints.school3grade8lower = dot(proportions(:,3),x(:,3)) >= 0.3*sum(x(:,3));
+prob.Constraints.school3grade8higher = dot(proportions(:,3),x(:,3)) <= 0.36*sum(x(:,3));
+[sol,fval]= solve(prob)
+sol.x
